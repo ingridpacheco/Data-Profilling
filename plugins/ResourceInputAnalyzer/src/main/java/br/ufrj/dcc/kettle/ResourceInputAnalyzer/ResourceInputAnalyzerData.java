2c70cf7ac86a340eeb6383e2e5e319b3674dd1f3
Ingrid Pacheco <ingrid.pacheco@corp.globo.com>
2020-07-16 23:00:49 -0300

    ✨ feat: add ResourceInputAnalyzer plugin

---
+++ plugins/ResourceInputAnalyzer/src/main/java/br/ufrj/dcc/kettle/ResourceInputAnalyzer/ResourceInputAnalyzerData.java
@@ -0,0 +1,34 @@
+package br.ufrj.dcc.kettle.ResourceInputAnalyzer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.trans.step.BaseStepData;
+import org.pentaho.di.trans.step.StepDataInterface;
+
+/**
+* @author IngridPacheco
+*
+*/
+
+public class ResourceInputAnalyzerData extends BaseStepData implements StepDataInterface {
+
+public RowMetaInterface outputRowMeta;
+	
+	int outputPropertyNameIndex = -1;
+	
+	List<String> csvProperties = new ArrayList<>();
+	List<String> properties = new ArrayList<>();
+	
+	List<String> notOnCSVProperties = new ArrayList<>();
+	
+	FileWriter CSVwriter;
+	BufferedWriter bufferedWriter;
+	
+	public ResourceInputAnalyzerData() {
+		super();
+	}
+
+}
