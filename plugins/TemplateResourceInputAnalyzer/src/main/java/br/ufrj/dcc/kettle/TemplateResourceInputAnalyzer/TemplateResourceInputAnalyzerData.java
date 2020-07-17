54cddb556d4b98680aa15a32c8d0c04dd34b3845
Ingrid Pacheco <ingrid.pacheco@corp.globo.com>
2020-07-16 23:25:56 -0300

    ✨ feat: add TemplateResourceInputAnalyzer plugin

---
+++ plugins/TemplateResourceInputAnalyzer/src/main/java/br/ufrj/dcc/kettle/TemplateResourceInputAnalyzer/TemplateResourceInputAnalyzerData.java
@@ -0,0 +1,37 @@
+package br.ufrj.dcc.kettle.TemplateResourceInputAnalyzer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.trans.step.BaseStepData;
+import org.pentaho.di.trans.step.StepDataInterface;
+
+/**
+* @author IngridPacheco
+*
+*/
+
+public class TemplateResourceInputAnalyzerData extends BaseStepData implements StepDataInterface {
+
+public RowMetaInterface outputRowMeta;
+	
+	int outputResourceNameIndex = -1;
+	int outputResourcesOnDBpediaIndex = -1;
+	
+	List<String> csvResources = new ArrayList<>();
+	List<String> resources = new ArrayList<>();
+	
+	List<String> resourcesFound = new ArrayList<>();
+	
+	FileWriter CSVwriter;
+	BufferedWriter bufferedWriter;
+	
+	Integer quantity = 0;
+	
+	public TemplateResourceInputAnalyzerData() {
+		super();
+	}
+
+}
